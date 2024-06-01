5c1d27ffaea5d44e6185c2f112102e9c74f8ea6c
Ravi <samcollins9899@gmail.com>
2024-06-02 01:34:39 +0530

    feat: caching service implemented in api

--- src/app/services/caching/caching.service.ts
+++ src/app/services/caching/caching.service.ts
@@ -1,24 +1,80 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, shareReplay } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CachingService {
-  private cache: { [key: string]: Observable<any> } = {};
+  private cache: Map<string, any>;
 
-  getObservable(key: string, observable: Observable<any>): Observable<any> {
-    if (!this.cache[key]) {
-      this.cache[key] = observable.pipe(shareReplay(1));
-    }
-    return this.cache[key];
+  constructor() {
+    this.cache = new Map<string, any>();
+    this.loadCacheFromLocalStorage();
   }
 
-  clearCache(key?: string) {
-    if (key) {
-      delete this.cache[key];
+  private loadCacheFromLocalStorage(): void {
+    const cacheData = localStorage.getItem('cache');
+    if (cacheData) {
+      this.cache = new Map<string, any>(JSON.parse(cacheData));
+    }
+  }
+
+  private saveCacheToLocalStorage(): void {
+    localStorage.setItem(
+      'cache',
+      JSON.stringify(Array.from(this.cache.entries()))
+    );
+  }
+
+  get(key: string): any {
+    return this.cache.get(key);
+  }
+
+  set(key: string, value: any): void {
+    this.cache.set(key, value);
+    this.saveCacheToLocalStorage();
+  }
+
+  has(key: string): boolean {
+    return this.cache.has(key);
+  }
+
+  clear(): void {
+    this.cache.clear();
+    localStorage.removeItem('cache');
+  }
+
+  getObservable<T>(key: string, fetch: Observable<T>): Observable<T> {
+    if (this.has(key)) {
+      return of(this.get(key));
     } else {
-      this.cache = {};
+      return fetch.pipe(tap((value) => this.set(key, value)));
     }
   }
 }
+
+// import { Injectable } from '@angular/core';
+// import { Observable, of, shareReplay } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class CachingService {
+//   private cache: { [key: string]: Observable<any> } = {};
+
+//   getObservable(key: string, observable: Observable<any>): Observable<any> {
+//     if (!this.cache[key]) {
+//       this.cache[key] = observable.pipe(shareReplay(1));
+//     }
+//     return this.cache[key];
+//   }
+
+//   clearCache(key?: string) {
+//     if (key) {
+//       delete this.cache[key];
+//     } else {
+//       this.cache = {};
+//     }
+//   }
+// }
